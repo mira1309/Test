cb2cd24857c9a950c22c7ab0e90cb1fb33166e28
Ильмира <kushkarova.1998@mail.ru>
2023-10-15 19:18:05 +0300

    finish code

--- src/main/java/com/calculate/calculateprogram/CalculatorController.java
+++ src/main/java/com/calculate/calculateprogram/CalculatorController.java
@@ -2,13 +2,41 @@ package com.calculate.calculateprogram;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
+
+    private final CalculateService calculateService;
+
+    public CalculatorController(CalculateService calculateService) {
+        this.calculateService = calculateService;
+    }
+
     @GetMapping
     public String greeting() {
-        return "Добро пожаловать в калькулятор!";
+        return calculateService.greeting();
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam int num1, @RequestParam int num2) {
+        return calculateService.plus(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return calculateService.minus(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multyply(@RequestParam int num1, @RequestParam int num2) {
+        return calculateService.multiply(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        return calculateService.divide(num1, num2);
     }
 }
