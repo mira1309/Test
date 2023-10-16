53968e95c2a1ce08952a82fc482493b02950c712
Ильмира <kushkarova.1998@mail.ru>
2023-10-16 11:34:58 +0300

    finish code with pom.xml

---
+++ src/main/java/com/calculate/calculateprogram/CalculateProgramApplication.java
@@ -0,0 +1,13 @@
+package com.calculate.calculateprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculate/calculateprogram/CalculateProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculate.calculateprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
